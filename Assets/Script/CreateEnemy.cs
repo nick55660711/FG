5cc32892ae8c772b299b0c124ebd4f3253e71a15
nick55660711 <63327482+nick55660711@users.noreply.github.com>
2020-06-11 13:49:42 +0800

    5

---
+++ Assets/Script/CreateEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    public GameObject Enemy;
+    public Transform Spawn1;
+    public bool SpwanON;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!SpwanON && other.tag == "Player")
+        {
+            createEnemy();
+            SpwanON = true;
+        }
+        
+    }
+    
+
+
+    public void createEnemy()
+    {
+        Instantiate(Enemy, Spawn1.position, Spawn1.rotation);
+
+    }
+
+
+
+
+
+
+
+
+
+}
